7d75ba5d16bce2b7a2744d8b92f855d12e7b2d43
unknown <ainsleyyyz@gmail.com>
2025-02-09 01:44:48 +0700

    test

---
+++ config/database.js
@@ -0,0 +1,12 @@
+//Mã nguồn được chia sẻ tại gamebai.dev
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   'game',
+		//'pass':   'game@789',
+		'dbName': 'rikfan', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
